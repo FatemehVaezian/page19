d4cf66bf76dbf28c7588a727c3cf02712a2aa0a6
fatemeh <1379ftov@gmail.com>
2024-09-08 14:35:20 +0330

    my commit

---
+++ vue.js
@@ -0,0 +1,36 @@
+new Vue({
+  el: "#my-vue-app",
+  data: {
+    title: "Social Widgets",
+
+    socialWidgetsNames: [
+      {
+        title: "Patrick Taylor",
+        text: "Patrick Taylor",
+        pic: "images/skype.png",
+      },
+      {
+        title: "Patrick Taylor",
+        text: "PatrickTaylor@gmail.com",
+        pic: "images/Gmail.png",
+      },
+      {
+        title: "@Patrick",
+        text: "PatrickTaylor@gmail.com",
+        pic: "images/twitter.png",
+      },
+      {
+        title: "Patrick Taylor",
+        text: "@Patrick_Taylor",
+        pic: "images/dribbble.png",
+      },
+      {
+        title: "Patrick Taylor",
+        text: "@Patrick_Taylor",
+        pic: "images/face.png",
+      },
+      { title: "", text: "Add Widget", pic: "images/plus.svg" },
+    ],
+  },
+  method: {},
+});
